498880713517867f693a4b639d44ab68de61370d
tsui <tsui@tutamail.com>
2025-09-05 14:09:03 +0800

    添加pm2管理启动方式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps : [{
     name   : "captcha-ocr",
     script : "server.py",
+    exec_mode: "cluster",
+    instances: 4,
     args: "",
     interpreter: "/root/miniconda3/envs/captcha/bin/python"
   }
